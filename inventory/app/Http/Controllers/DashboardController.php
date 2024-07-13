9d15fa1813971f1c005eb990a2e1472e6fd838ed
Yujin14 <106482110+Yujin14@users.noreply.github.com>
2024-07-13 11:35:34 +0800

    Total Count of Users

--- inventory/app/Http/Controllers/DashboardController.php
+++ inventory/app/Http/Controllers/DashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\DefectiveModel;
+use App\Models\DeviceModel;
 use App\Models\User;
 use Inertia\Inertia;
 
@@ -13,10 +14,12 @@ class DashboardController extends Controller
         // Retrieve total defective count and user count from the database
         $defectiveCount = DefectiveModel::count();
         $totalUserCount = User::count();
+        $deviceCount = DeviceModel::count();
 
         return Inertia::render('Dashboard/Index', [
             'defectiveCount' => $defectiveCount,
-            'totalUserCount' => $totalUserCount
+            'totalUserCount' => $totalUserCount,
+            'deviceCount' => $deviceCount
         ]);
     }
 
